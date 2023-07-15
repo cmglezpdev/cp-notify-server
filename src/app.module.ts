db5e957434bd839f2871c9dfcdfce83f2bb2d68a
Carlos Manuel González Peña <cmglezpdev@gmail.com>
2023-07-15 16:05:52 -0400

    fix: update app module with Platform module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,31 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { ContestModule } from './contest/contest.module';
 import { JudgesModule } from './judges/judges.module';
 import { ProfileModule } from './profile/profile.module';
+import { PlatformModule } from './platform/platform.module';
+import { Platform } from './platform/entities/platform.entity';
+import { Contest } from './contest/entities/contest.entity';
 
 @Module({
-  imports: [ContestModule, JudgesModule, ProfileModule],
   controllers: [],
   providers: [],
+  imports: [
+    ContestModule, 
+    JudgesModule, 
+    ProfileModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'cp-notify-development-database',
+      database: 'CPNotify',
+      entities: [Platform, Contest],
+      synchronize: true,
+      autoLoadEntities: true
+    }),
+    PlatformModule
+  ],
 })
 export class AppModule {}
