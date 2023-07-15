3ebdd7d3fd85a7de8cc34139255ab34d2257ec42
Carlos Manuel González Peña <cmglezpdev@gmail.com>
2023-07-15 16:06:07 -0400

    feat: add Contest Entity

--- src/contest/entities/contest.entity.ts
+++ src/contest/entities/contest.entity.ts
@@ -1,1 +1,26 @@
-export class Contest {}
+import { Column, Entity, PrimaryColumn, ManyToOne } from "typeorm";
+import { Platform } from '../../platform/entities/platform.entity';
+
+@Entity()
+export class Contest {
+    @PrimaryColumn('text')
+    id: string;
+
+    @Column('text', { nullable: false })
+    name: string;
+
+    @Column('int', { nullable: false })
+    durationSeconds:  number;
+
+    @Column('int', { nullable: false })
+    startTimeSeconds: number;
+    
+    @Column('text', { nullable: false })
+    link:             string;
+
+    @Column('text')
+    type?: string;
+
+    @ManyToOne(() => Platform)
+    platform: string;
+}
