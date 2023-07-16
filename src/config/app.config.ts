8805f5f6f69de49bc799182a5934433b7e8386ee
Carlos Manuel González Peña <cmglezpdev@gmail.com>
2023-07-16 17:04:13 -0400

    feat: add schema to validate env

---
+++ src/config/app.config.ts
@@ -0,0 +1,12 @@
+import * as Joi from 'joi';
+
+
+export const JoiValidationSchema = Joi.object({
+    DB_HOST: Joi.required(),
+    DB_PORT: Joi.number().required(),
+    DB_USERNAME: Joi.required(),
+    DB_PASSWORD: Joi.required(),
+    DB_NAME: Joi.required().default('CPNotify'),
+    PORT: Joi.number().default(4000),
+});
+
