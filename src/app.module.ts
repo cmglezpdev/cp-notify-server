b250295018cf3a4480caa924501872d7741ae40f
Carlos Manuel González Peña <cmglezpdev@gmail.com>
2023-07-16 17:04:33 -0400

    refact: use environment variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,26 +9,31 @@ import { PlatformModule } from './platform/platform.module';
 
 import { Platform } from './platform/entities/platform.entity';
 import { Contest } from './contest/entities/contest.entity';
+import { ConfigModule } from '@nestjs/config';
+import { JoiValidationSchema } from './config/app.config';
 
 @Module({
   controllers: [],
   providers: [],
   imports: [
-    ContestModule, 
-    JudgesModule, 
-    ProfileModule,
+    ConfigModule.forRoot({
+      validationSchema: JoiValidationSchema
+    }),
     ScheduleModule.forRoot(),
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: 'cp-notify-development-database',
-      database: 'CPNotify',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
       entities: [Platform, Contest],
       synchronize: true,
       autoLoadEntities: true
     }),
+    ContestModule, 
+    JudgesModule, 
+    ProfileModule,
     PlatformModule,
   ],
 })
