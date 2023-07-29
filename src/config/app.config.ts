d64b517a0799f51ae07c011d4dbc11d6b82e989d
Carlos Manuel González Peña <cmglezpdev@gmail.com>
2023-07-29 14:02:07 -0400

    feat: add JWT_SECRET to the .env validaton

--- src/config/app.config.ts
+++ src/config/app.config.ts
@@ -8,5 +8,6 @@ export const JoiValidationSchema = Joi.object({
     DB_PASSWORD: Joi.required(),
     DB_NAME: Joi.required().default('CPNotify'),
     PORT: Joi.number().default(4000),
+    JWT_SECRET: Joi.required()
 });
 
